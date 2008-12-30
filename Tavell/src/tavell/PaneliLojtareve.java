18163afada7c7f8a8e3e162e6eac750841bfa546
arberceni <arberceni@523e91e3-130c-4d77-ac04-084eeeb02a07>
2008-12-30 01:44:44 +0000

    Shtova paraqitjen e pikeve per cdo lojtar si dhe ndertova dhe nje konstruktore tjeter overload, i cili merr si parametra 2 Lojtare dhe 2 avatar. Kjo per paraqitjen e sakte me vone. Aktualisht perdoret konstruktori pa parametra i cili i ka te dhenat per efekt ilustrimi.

--- Tavell/src/tavell/PaneliLojtareve.java
+++ Tavell/src/tavell/PaneliLojtareve.java
@@ -30,6 +30,10 @@ public class PaneliLojtareve extends JPanel{
     private JLabel emriPips2;
     private JLabel pips1;
     private JLabel pips2;
+    private JLabel emriPike1;
+    private JLabel emriPike2;
+    private JLabel pike1;
+    private JLabel pike2;    
     private JButton ruajLojen;
     private JButton lojeRe;
     private JButton ngarko;
@@ -65,6 +69,12 @@ public class PaneliLojtareve extends JPanel{
         
         pips1 = new JLabel("123"); //Do behet lojtar.getPips();
         pips1.setBounds(60, 110, 100, 15);
+        
+        emriPike1 = new JLabel("Piket: ");
+        emriPike1.setBounds(10, 130, 100, 15);
+        
+        pike1 = new JLabel("3"); // Do behet lojtar.getPike();
+        pike1.setBounds(60, 130, 100, 15);
                 
         paneliLojtarit1.add(emri1);
         paneliLojtarit1.add(emriLojarit1);
@@ -72,6 +82,8 @@ public class PaneliLojtareve extends JPanel{
         paneliLojtarit1.add(avatar1);
         paneliLojtarit1.add(emriPips1);
         paneliLojtarit1.add(pips1);
+        paneliLojtarit1.add(emriPike1);
+        paneliLojtarit1.add(pike1);
         
         //Paneli Butonave
         paneliButonave = new JPanel(new FlowLayout(FlowLayout.LEFT, 5, 5));        
@@ -111,12 +123,126 @@ public class PaneliLojtareve extends JPanel{
         pips2 = new JLabel("133"); //Do behet lojtar2.merrPips();
         pips2.setBounds(60, 110, 100, 15);
         
+        emriPike2 = new JLabel("Pike: ");
+        emriPike2.setBounds(10, 130, 100, 15);
+        
+        pike2 = new JLabel("5");
+        pike2.setBounds(60, 130, 100, 15);
+        
         paneliLojtarit2.add(emri2);
         paneliLojtarit2.add(emriLojarit2);
         paneliLojtarit2.add(emriAvatar2);
         paneliLojtarit2.add(avatar2);
         paneliLojtarit2.add(emriPips2);
         paneliLojtarit2.add(pips2);
+        paneliLojtarit2.add(emriPike2);
+        paneliLojtarit2.add(pike2);
+        
+        
+        this.add(paneliLojtarit2); 
+        this.add(paneliButonave);
+        this.add(paneliLojtarit1);        
+        
+        //-------------FUND PANELI LOJTAREVE----------------------        
+    }
+    
+    public PaneliLojtareve(Lojtar l1,Lojtar l2, AvatarImage avatar1, AvatarImage avatar2) {        
+        
+        this.setBounds(Fushe.WIDTH+5,0,1100-Fushe.WIDTH-15,Fushe.HEIGHT);
+        this.setBorder(new LineBorder(Color.black));
+        this.setLayout(null);
+        
+        //-------------PANELI LOJTAREVE---------------------------        
+                
+        //Paneli i Lojtarit 1
+        paneliLojtarit1 = new JPanel(null);        
+        paneliLojtarit1.setBounds(10,20,200,250);
+        paneliLojtarit1.setBorder(new TitledBorder("Lojtari 1"));        
+             
+        emri1 = new JLabel("Emri: ");
+        emri1.setBounds(10, 40, 100, 15);
+        
+        emriLojarit1 = new JLabel(l1.getEmri());
+        emriLojarit1.setBounds(50, 40, 200, 15);
+        
+        emriAvatar1 = new JLabel("Avatar: ");
+        emriAvatar1.setBounds(10, 70, 100, 15);
+        
+        avatar1.setBounds(60, 60, 40, 40);
+        
+        emriPips1 = new JLabel("Pips: ");
+        emriPips1.setBounds(10, 110, 100, 15);
+        
+        pips1 = new JLabel(String.valueOf(l1.getPips()));
+        pips1.setBounds(60, 110, 100, 15);
+        
+        emriPike1 = new JLabel("Piket: ");
+        emriPike1.setBounds(10, 130, 100, 15);
+        
+        pike1 = new JLabel(String.valueOf(l1.getPike()));
+        pike1.setBounds(60, 130, 100, 15);
+                
+        paneliLojtarit1.add(emri1);
+        paneliLojtarit1.add(emriLojarit1);
+        paneliLojtarit1.add(emriAvatar1);
+        paneliLojtarit1.add(avatar1);
+        paneliLojtarit1.add(emriPips1);
+        paneliLojtarit1.add(pips1);
+        paneliLojtarit1.add(emriPike1);
+        paneliLojtarit1.add(pike1);
+        
+        //Paneli Butonave
+        paneliButonave = new JPanel(new FlowLayout(FlowLayout.LEFT, 5, 5));        
+        paneliButonave.setBounds(10,275,200,120);
+        paneliButonave.setBorder(new TitledBorder(""));
+        
+        ruajLojen = new JButton("Ruaj");
+        lojeRe = new JButton("Loje e Re");
+        ngarko = new JButton("Ngarko");
+        dorezohu = new JButton("Dorezohu");
+        
+        paneliButonave.add(ruajLojen);
+        paneliButonave.add(lojeRe);
+        paneliButonave.add(ngarko);
+        paneliButonave.add(dorezohu);
+        
+        //Paneli Lojtarit2
+        paneliLojtarit2 = new JPanel(null);        
+        paneliLojtarit2.setBounds(10,400,200,250);
+        paneliLojtarit2.setBorder(new TitledBorder("Lojtari 2"));
+        
+        emri2 = new JLabel("Emri: ");
+        emri2.setBounds(10, 40, 100, 15);
+        
+        emriLojarit2 = new JLabel(l2.getEmri());
+        emriLojarit2.setBounds(50, 40, 200, 15);
+        
+        emriAvatar2 = new JLabel("Avatar: ");
+        emriAvatar2.setBounds(10, 70, 100, 15);
+        
+        //avatar2 = new AvatarImage("female.jpg");
+        avatar2.setBounds(60, 60, 40, 40);
+        
+        emriPips2= new JLabel("Pips: ");
+        emriPips2.setBounds(10, 110, 100, 15);
+        
+        pips2 = new JLabel(String.valueOf(l2.getPips()));
+        pips2.setBounds(60, 110, 100, 15);
+        
+        emriPike2 = new JLabel("Pike: ");
+        emriPike2.setBounds(10, 130, 100, 15);
+        
+        pike2 = new JLabel(String.valueOf(l2.getPike()));
+        pike2.setBounds(60, 130, 100, 15);
+        
+        paneliLojtarit2.add(emri2);
+        paneliLojtarit2.add(emriLojarit2);
+        paneliLojtarit2.add(emriAvatar2);
+        paneliLojtarit2.add(avatar2);
+        paneliLojtarit2.add(emriPips2);
+        paneliLojtarit2.add(pips2);
+        paneliLojtarit2.add(emriPike2);
+        paneliLojtarit2.add(pike2);
         
         
         this.add(paneliLojtarit2); 
@@ -133,5 +259,13 @@ public class PaneliLojtareve extends JPanel{
     
     public void setPips2(int pips){
         this.pips2.setText(String.valueOf(pips));       
+    }
+    
+    public void setPike1(int pike){
+        this.pike1.setText(String.valueOf(pike));        
+    }
+    
+    public void setPike2 (int pike){
+        this.pike2.setText(String.valueOf(pike));
     }
 }
